1975b4d480ae479c2ed3b18d4b00094cf2124368
Wave <Samjpoyner@icloud.com>
2024-05-28 17:50:13 +0000

    Add files via upload

---
+++ test.py
@@ -0,0 +1,30 @@
+import neural
+import numpy as np
+import matplotlib.pyplot as plt
+
+input_vectors=np.array(
+	[
+		[3, 1.5],
+		[2,1],
+		[4, 1.5],
+		[3, 4],
+		[3.5, 0.5],
+		[2, 0.5],
+		[5.5, 1],
+		[1, 1],
+	]
+)
+
+targets=np.array([0,1,0,1,0,1,1,0])
+learning_rate=0.1
+
+neural_network=neural.NeuralNetwork(learning_rate)
+training_error=neural_network.train(input_vectors, targets, 10000)
+
+plt.plot(training_error)
+plt.xlabel("Iterations")
+plt.ylabel("Error for training instances")
+plt.savefig("cumulative_error.png")
+
+#print(neural_network.predict([6, 3]))
+#print(neural_network.weights)
